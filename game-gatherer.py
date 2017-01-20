c1fc16dc478da034cb6b74d6e9bca90fb57546e8
Leon Springer <leon.springer@rootmetrics.com>
2017-01-20 15:55:44 -0800

    added controls around html file creation

--- game-gatherer.py
+++ game-gatherer.py
@@ -9,12 +9,13 @@ import datetime
 
 
 video_title = []
+# out_file = 'gamelinks.html' 
 
 #url = "http://www.fullmatchesandshows.com"
 #url = "http://www.fullmatchesandshows.com/2017/01/08/villarreal-vs-barcelona-highlights-full-match-2/"
 
 
-def get_links(url):
+def get_links(url, verbose):
     page = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
     soup = BeautifulSoup(page.content)
     if url == "http://www.fullmatchesandshows.com" or url == "http://www.fullmatchesandshows.com/":
@@ -27,7 +28,7 @@ def get_links(url):
             urls.append(url.attrs['href'])
         for url in urls:
             sub_urls = []
-            print ('[+] Checking: {0}'.format(url)) 
+            print ('[+] Checking: {0}'.format(url))
             page = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
             sub_soup = BeautifulSoup(page.content)
             # Get first content on link on first page (typically a highlight)
@@ -38,7 +39,8 @@ def get_links(url):
                 sub_url = item.find("a")
                 sub_urls.append(sub_url.attrs['href'])
             for sub_url in sub_urls:
-                print ('[+] Getting additional content: {0}'.format(sub_url)) 
+                if verbose:
+                    print ('[+] Getting additional content: {0}'.format(sub_url)) 
                 page = requests.get(sub_url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
                 sub_soup = BeautifulSoup(page.content)
                 for post_content in sub_soup.find_all("div", class_="td-post-content"):
@@ -57,7 +59,8 @@ def get_links(url):
             sub_url = item.find("a")
             sub_urls.append(sub_url.attrs['href'])
         for sub_url in sub_urls:
-            print ('[+] Checking: {0}'.format(sub_url)) 
+            if verbose:
+                print ('[+] Checking: {0}'.format(sub_url)) 
             page = requests.get(sub_url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
             sub_soup = BeautifulSoup(page.content)
             for post_content in sub_soup.find_all("div", class_="td-post-content"):
@@ -103,28 +106,49 @@ def parse_remote_json(urls):
             print('Json url malformed: {0}'.format(urls))
         return json_content
 
-def run(url):
+def run(url, html_off, out_file, verbose):
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    data_configs = get_links(url)
+    data_configs = get_links(url, verbose)
     json_urls, video_urls = build_urls(data_configs)
     json_content = parse_remote_json(json_urls)
     video_title = print_json_object(json_content)
-    html_file = open('matchlinks.htm',"a+")
-    for v_url, v_title in zip(video_urls, video_title):
-        print v_title
-        print v_url
-        print "--------------"
-        html_file.write('{0}   -   <a href ="{1}">{2}</a><br/>\n'.format(now, v_url, v_title))
-    html_file.close()
+    if not html_off:
+        if verbose:
+            if type(out_file)==str:
+                print "Writing output file to: {0}".format(out_file)
+            else:
+                print "Writing output file to: {0}".format(out_file[0])
+        if type(out_file)==str:
+            html_file = open(out_file,"a+")
+        else:
+            html_file = open(out_file[0],"a+")
+        for v_url, v_title in zip(video_urls, video_title):
+            print v_title
+            print v_url
+            print "--------------"
+            if not html_off:
+                html_file.write('{0}   -   <a href ="{1}">{2}</a><br/>\n'.format(now, v_url, v_title))
+        html_file.close()
+    else:
+        for v_url, v_title in zip(video_urls, video_title):
+            print v_title
+            print v_url
+            print "--------------"
             
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()     
+    parser.add_argument('-f', '--file', nargs=1, default='links.html', dest='out_file', help='Output html file name and location')
+    parser.add_argument('-o', '--html_off', action='store_true', default=False, help='Disable html file output')
+    parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('url', nargs='?', default='http://www.fullmatchesandshows.com')
+    args = parser.parse_args()
     return parser.parse_args()
 
 if __name__ == '__main__':
     parser = parse_args()
-    links = run(parser.url)
+    links = run(parser.url, parser.html_off, parser.out_file, parser.verbose)
+    
+
 
 
 
